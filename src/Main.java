f026bc4381bb664c029a2fde2aeec670ac425e32
xunmeng <1316289271@qq.com>
2024-04-29 23:33:37 +0800

    add class A and test it

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         A temp = new A();
         temp.show();
+        A temp1 = new A();
+        temp1.a = 2;
+        temp1.b = 2.2;
+        temp1.show();
     }
 }

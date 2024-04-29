778d31319fd6ff1642e9122c5a26ff292f44639e
xunmeng <1316289271@qq.com>
2024-04-29 23:28:16 +0800

    add class A and test it

---
+++ src/A.java
@@ -0,0 +1,15 @@
+public class A {
+    public int a;
+    public double b;
+    public A()
+    {
+        a = 0;
+        b = 0;
+    }
+
+    public void show()
+    {
+        System.out.println(a + " " + b);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        A temp = new A();
+        temp.show();
     }
 }
